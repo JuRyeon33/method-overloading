3653b903bebacd677cd90b85a3c5b15438441b5a
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-09 09:35:33 +0900

    first method-overloading

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
-    public static void main(String[] args) {
 
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("a + b =" + add(1, 2));
+        System.out.println("a + b + c = " + add(1, 2, 3));
+        System.out.println("a + b = " + add(2.5, 3.5));
     }
 }
